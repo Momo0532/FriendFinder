996df10cf9e85dc4370074b08d795f2450ed9ec9
jason villaluz <jasonvillaluz@jasons-mbp.lan>
2018-12-17 07:47:22 -0800

    first set of files

---
+++ app/data/friends.js
@@ -0,0 +1,29 @@
+var friends = [
+	{
+		name: "Moe Szyslak",
+		photo: "https://en.wikipedia.org/wiki/Moe_Szyslak#/media/File:Moe_Szyslak.png",
+		scores: [5, 1, 2, 3, 1, 2, 5, 1, 1, 1]
+	},
+	{
+		name: "Barney Gumble",
+		photo: "https://vignette.wikia.nocookie.net/simpsons/images/6/68/Barney_Gumble_-_shading.png/revision/latest?cb=20180313200146",
+		scores: [2, 5, 4, 1, 2, 3, 3, 2, 3, 3]
+	},
+	{
+		name: "Lenny Leonard",
+		photo: "https://vignette.wikia.nocookie.net/the-simpsons/images/a/ae/Lenny_Leonard.png/revision/latest?cb=20140403022959",
+		scores: [1, 1, 5, 3, 3, 5, 1, 2, 3, 4]
+	},
+	{
+		name: "Carl Carlson",
+		photo: "https://vignette.wikia.nocookie.net/simpsons/images/c/c6/Carl_Carlson_-_shading.png/revision/latest?cb=20180312061955",
+		scores: [3, 1, 3, 5, 2, 3, 2, 2, 5, 5]
+	},
+	{
+		name: "Lurleen Lumpkin",
+		photo: "https://vignette.wikia.nocookie.net/simpsons/images/d/d5/Lurleen_Lumpkin_Tapped_Out.png/revision/latest?cb=20151223041019",
+		scores: [3, 1, 4, 4, 2, 4, 2, 5, 1, 3]
+	}
+];
+
+module.exports = friends;
