9ff6d05db540c935508c928440c820a89c0d8e2f
jason villaluz <jasonvillaluz@jasons-MacBook-Pro.local>
2018-12-17 10:48:59 -0800

    pudates

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 // Dependencies
-var = require("express");
+var express = require("express");
 var bodyParser = require("body-parser");
 var path = require("path");
 
@@ -10,10 +10,10 @@ var PORT = process.env.PORT || 8080;
 // For serving of static CSS
 app.use(express.static(__dirname + "/app/css"));
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.text());
-app.use(bodyParser.json({ type: "application/vnd.api+json" }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+// app.use(express.text());
+// app.use(express.json({ type: "application/vnd.api+json" }));
 
 // API and HTML routes
 require("./app/routing/apiRoutes.js")(app);
